de6bd02f9c3a09b40859be0afc7afe432d7ba131
myke <81974207+heismyke@users.noreply.github.com>
2022-08-13 05:24:48 +0100

    recent

---
+++ APp/app.js
@@ -0,0 +1,66 @@
+// var app = angular.module('apps', []);
+// // apps.config(function(){
+
+// // })
+
+// // apps.run(function(){
+
+// // })
+
+// app.controller('appController', function($scope){
+
+//     $scope.remove = function(name){
+//         var removed = $scope.names.indexOf(name);
+//         $scope.names.splice(removed, 1);    
+    
+//     }
+
+//     $scope.NewUser = function(){
+//         $scope.names.push({
+//             name: $scope.newUser.name,
+//             sex: $scope.newUser.sex,
+//             age:parseInt($scope.newUser.age),
+//         });
+//     };
+//     $scope.message = 'hey, my names are: ';
+//     $scope.names = [
+//         {
+//            name: "michael",
+//            age: 20,
+           
+//         },
+
+//         {
+//             name: "kelvin",
+//             age: 40,
+          
+//         },
+
+//         {
+//             name: "teddy",
+//             age: 60,
+         
+//         },
+
+//         {
+//             name: "emi",
+//             age: 50,
+            
+//         },
+//         {
+//             name: "gabriel",
+//             age: 20,
+            
+
+//          },
+ 
+         
+         
+//     ];
+
+// })
+var app = angular.module('postalApp', []);
+
+app.contoller('postalController', function($scope){
+    
+})
